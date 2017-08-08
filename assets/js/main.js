f80c7eed7c88b4d83a97666d9da86a4c604eed23
kelyanamuro <kelyanamurohuamani@gmail.com>
2017-08-08 10:51:02 -0500

    Añadiendo funcionalidades

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,16 @@ var mapa=[
 for(var i = 0; i<mapa.length;i++){
     var string = mapa[i].split("");
     var TR = document.createElement("tr");
+
     for(var j =0; j<string.length; j++){
         var TD = document.createElement("td");
         TD.setAttribute("id", i+""+j);
-        
+
+//appemdChild = añade un nodo como el último hijo de un nodo.        
         var text = document.createTextNode(string[j]);
         TD.appendChild(text);
         TR.appendChild(TD);
+
         if(string[j]=="*"){
             TD.setAttribute("class", "negro");
         }else if(string[j]=="_"){
@@ -31,24 +34,27 @@ for(var i = 0; i<mapa.length;i++){
             TD.setAttribute("class", "final");
         }else{
             TD.setAttribute("class", "inicio");
-            var R = document.createElement("div");
-            R.setAttribute("id", "redondo");
-            TD.appendChild(R);
+            var crearMovil = document.createElement("div");
+            crearMovil.setAttribute("id", "redondo");
+            TD.appendChild(crearMovil);
         }
     }
     tabla.appendChild(TR);
 }
 
 window.onload=function(){
-    document.onkeydown=desplazar;
+    document.onkeydown=desplazar;//evento ONKEYDOWN se produce cuando se presiona una tecla.
 };
-    function desplazar(objeto){
-    var tecla = objeto.which;
-    var tag_padre = document.getElementById("redondo").parentNode.id;
+
+function desplazar(objeto){
+    var tecla = objeto.which; //Reconoce la tecla del teclado.
+    var tag_padre = document.getElementById("redondo").parentNode.id;//devuelve el nodo padre del nodo especificado.
     var separa = tag_padre.split("");
+
     if (separa.length == 3) {
         separa[1] = separa[1]+""+separa[2];
     }
+
     var arriba = (separa[0] - 1)+ ""+separa[1];
     var abajo = (parseInt(separa[0])+1)+""+separa[1];
     var derecha = separa[0]+ "" + (parseInt(separa[1])+1);
@@ -67,9 +73,9 @@ window.onload=function(){
                     redondo.style.left = situacionY-220+"px";
                     var contenido = document.getElementById(tag_padre);
                     contenido.removeChild(contenido.childNodes[1]);
-                    var R = document.createElement("div");
-                    R.setAttribute("id", "redondo");
-                    document.getElementById(izquierda).appendChild(R); 
+                    var crearMovil = document.createElement("div");
+                    crearMovil.setAttribute("id", "redondo");
+                    document.getElementById(izquierda).appendChild(crearMovil); 
                 }
                 break;
 
@@ -78,9 +84,9 @@ window.onload=function(){
                     redondo.style.top = situacionX-220+"px";
                     var contenido = document.getElementById(tag_padre);
                     contenido.removeChild(contenido.childNodes[1]);
-                    var R = document.createElement("div");
-                    R.setAttribute("id", "redondo");
-                    document.getElementById(arriba).appendChild(R);
+                    var crearMovil = document.createElement("div");
+                    crearMovil.setAttribute("id", "redondo");
+                    document.getElementById(arriba).appendChild(crearMovil);
                 }
                 break;
 
@@ -89,9 +95,9 @@ window.onload=function(){
                     redondo.style.left = situacionY-180+"px";
                     var contenido = document.getElementById(tag_padre);
                     contenido.removeChild(contenido.childNodes[1]);
-                    var R = document.createElement("div");
-                    R.setAttribute("id", "redondo");
-                    document.getElementById(derecha).appendChild(R);
+                    var crearMovil = document.createElement("div");
+                    crearMovil.setAttribute("id", "redondo");
+                    document.getElementById(derecha).appendChild(crearMovil);
                 }
                 break;
 
@@ -100,15 +106,15 @@ window.onload=function(){
                     redondo.style.top = situacionX-180+"px";
                     var contenido = document.getElementById(tag_padre);
                     contenido.removeChild(contenido.childNodes[1]);
-                    var R = document.createElement("div");
-                    R.setAttribute("id", "redondo");
-                    document.getElementById(abajo).appendChild(R);
+                    var crearMovil = document.createElement("div");
+                    crearMovil.setAttribute("id", "redondo");
+                    document.getElementById(abajo).appendChild(crearMovil);
                 }
-                break;
+                break; 
             default:
                 console.log("pulse otra tecla :(");
         }
-    }
+}
 
 /* 
 var teclas = {
